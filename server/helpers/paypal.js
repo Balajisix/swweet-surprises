81e577b948ae9053a8108a5a952650b871fbd585
Balajisix <102216943+Balajisix@users.noreply.github.com>
2024-12-09 20:06:13 +0530

    Update paypal.js

--- server/helpers/paypal.js
+++ server/helpers/paypal.js
@@ -2,8 +2,8 @@ const paypal = require("paypal-rest-sdk");
 
 paypal.configure({
   mode: "sandbox",
-  client_id: "ASdQGlHAb3nrHOhJi59772cDBB6KH50VdVpkMXSeyMpDhzQgF_s-Y1MKLeLWqKfBA-aXtcBTXZWwQpWt",
-  client_secret: "EIZmnYJ8UnrvatzGq10_QsuE5jnB1dyD_mpP5XousUq0w9QjEpkUDU2CXBn6HCI3gKj2NG4iZaJ_Ofsp",
+  client_id: "",
+  client_secret: "",
 });
 
 module.exports = paypal;
