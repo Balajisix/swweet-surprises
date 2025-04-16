a3a7d29ee9763b04c24eb0384ade0beab8adb1c9
Balaji <balajivs0305@gmail.com>
2025-04-16 17:26:48 +0530

    fixing refresh issue

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const path = require('path');
 const dotenv = require("dotenv")
 const authRouter = require("./routes/auth/auth-routes");
 const adminProductsRouter = require("./routes/admin/products-routes");
@@ -49,6 +50,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRouter);
@@ -65,5 +70,5 @@ app.use("/api/shop/review", shopReviewRouter);
 
 app.use("/api/common/feature", commonFeatureRouter);
 
-// app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
-module.exports = app;
+app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
+// module.exports = app;
